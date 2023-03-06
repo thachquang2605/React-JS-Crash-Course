6131e26a27d4a25efbf41ade54be0d1460911c18
thach quang <thachmessi198@gmail.com>
2023-03-06 16:05:52 +0700

    addtask.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 const App = () => {
+
+  const [showAddTask, setShowAddTask]= 
+  useState(false)
   const [tasks, setTasks] = useState([
     {
       "id": 1,
@@ -23,11 +27,47 @@ const App = () => {
     }
   ])
 
+  //Add task
+  const addTask=(task) =>{
+   // console.log(task)
+   const id = Math.floor(Math.random() *
+   10000 ) + 1
+
+   const newTask = { id,...task }
+   setTasks([...tasks, newTask])
+  }
+// delete task
+ const deleteTask = (id) =>{
+   //console.log('delete', id)
+   setTasks(tasks.filter((task) => task.id !== id))
+ }
+
+ //Toggle reminder
+ const toggleReminder = (id) => {
+  //console.log(id)
+   setTasks(
+    tasks.map((task) =>
+    task.id === id ? {...task, reminder:
+    !task.reminder} : task
+   )
+  )
+
+ } 
   return (
     <div className='container'>
-      <Header />
-      <Tasks tasks={tasks} />
-    </div>
+      <Header 
+       onAdd={()=> setShowAddTask(
+        !showAddTask)} 
+        showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd ={addTask}/>}
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} 
+      onDelete={deleteTask}
+       onToggle={toggleReminder}
+      />) :(
+        'no Tasks to Show'
+      )
+}</div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
 
 import PropTypes from 'prop-types'
 import Button from './Button'
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({ title, onAdd, showAdd   }) => {
+   
+    
     return (
         <header className='header'>
             <h1 > {title} </h1>
-            <Button color='green' text='Add'
-                onClick={onClick} />
+            <Button 
+            color={showAdd ? 'red': 'green'}
+             text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
 
         </header>
     )
 }
 Header.defaultProps = {
-    title: 'Dang Nhap',
+    title: 'Tracker',
 }
 Header.propTypes = {
     title: PropTypes.string.isRequired,
